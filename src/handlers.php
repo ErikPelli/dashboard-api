3cb153de12e2201ba6dd15402f885dd2a43a8415
Erik Pellizzon <erikpellizzon1@gmail.com>
2022-04-28 21:36:48 +0200

    Update handlers.php

--- src/handlers.php
+++ src/handlers.php
@@ -6,7 +6,7 @@ const functionHandlers = [
 ];
 
 function showError(string $message, int $statusCode) {
-    http_response_code($code);
+    http_response_code($statusCode);
     $error = array(
         "success" => false,
         "error" => $message,
