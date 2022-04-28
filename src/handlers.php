936a14419853a332540a02fb4a1336c6faa41eb0
ErikPelli <erikpellizzon1@gmail.com>
2022-04-28 22:06:22 +0200

    Example registration handler

--- src/handlers.php
+++ src/handlers.php
@@ -1,8 +1,33 @@
 <?php
 namespace Src;
 
-const functionHandlers = [
+$functionHandlers = [
+    "utente" => array(
+        "GET" => function(RequestHandler $r) {
+            // Get user information
 
+        },
+        "POST" => function(RequestHandler $r) {
+            // Register
+            $full = $r->data["fullname"];
+            $email = $r->data["email"];
+            $username = $r->data["username"];
+            $password = $r->data["password"];
+
+            $result = $r->db->registerUser($full, $email, $username, $password);
+            $error = $r->db->getError();
+            if($error) {
+                showError($error, 406);
+            } else {
+                http_response_code(200);
+                $error = array(
+                    "success" => true,
+                    "result" => $result,
+                );
+                echo json_encode($error, JSON_FORCE_OBJECT);
+            }
+        },
+    ),
 ];
 
 function showError(string $message, int $statusCode) {
