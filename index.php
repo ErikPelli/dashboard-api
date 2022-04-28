54bd0ee558b293b600b6c82e0967109f245e6173
ErikPelli <erikpellizzon1@gmail.com>
2022-04-28 17:24:11 +0200

    Add index file

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+require "src/bootstrap.php";
+use Src\Controller\CustomerController;
+
+header("Content-Type: application/json");
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri = explode( '/', $uri );
+
+// Check endpoint
+if ($uri[1] !== 'api' || !isset($uri[2]) || !isset($uri[3])) {
+    $error = array(
+        "success" => false,
+        "error" => "Invalid endpoint",
+        "result" => array(),
+    );
+    echo json_encode($error, JSON_FORCE_OBJECT);
+    exit();
+}
+
+// email
+$userId = $uri[2];
+
+// REST API name
+$function = $uri[3];
+
+// GET, POST, PUT, DELETE
+$requestMethod = $_SERVER["REQUEST_METHOD"];
