98c413dca44ac162463f96ac6fc42bcb7d33b47d
BlackJekko <ilias13082003@gmail.com>
2022-04-29 09:51:42 +0200

    Update DatabaseHandler.php

--- src/DatabaseHandler.php
+++ src/DatabaseHandler.php
@@ -3,10 +3,26 @@
 namespace Src;
 
 class DatabaseHandler {
-    private $db;
+    private \mysqli $db;
 
     public function __construct($db) {
         $this->db = $db;
+    }
+
+    public function error() {
+        $err = $this->db->error;
+        if ($err == "") {
+            $err = null;
+        }
+        return $err;
+    }
+
+    public function infoUser($email){
+        $sql = "SELECT firstName, lastName FROM PersonalData JOIN Employee ON PersonalData.fiscalCode = Employee.fiscalCode WHERE email = $email";
+        return $this->db->query($sql);
+    }
+    public function registerUser($){
+
     }
 
     public function close() {
